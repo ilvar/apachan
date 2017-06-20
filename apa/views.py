7801d784fcf5e3f96ec673e8e28f2d922c9d03d8
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2017-06-20 23:08:18 +0100

    Single Lenta update

--- apa/views.py
+++ apa/views.py
@@ -475,10 +475,15 @@ class CaptchaCommentView(DetailView):
         if self.request.session.get('post_captcha') == self.request.POST.get('captcha'):
             Comment.objects.filter(pk=comment.pk).update(rating=0)
             c = Comment.objects.filter(pk=comment.pk)[0]
+
+            update_data = {
+                'hidden': 0,
+                'replies': F('replies') + 1
+            }
             if not c.dont_raise:
-                Lenta.objects.filter(root=c.root_id).update(datetime=to_datetime())
-            Lenta.objects.filter(root_id=comment.pk).update(hidden=0)
-            Lenta.objects.filter(root_id=comment.root_id).update(replies=F('replies') + 1)
+                update_data['datetime'] = to_datetime()
+
+            Lenta.objects.filter(root_id=comment.pk).update(**update_data)
 
             messages.success(request, u'Камент запощен.')
             return HttpResponseRedirect('/%s.html#comment-%s' % (comment.root_id or comment.pk, comment.pk))
