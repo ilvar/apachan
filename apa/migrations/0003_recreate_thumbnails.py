0f2f239a89b643d6692f07a848d4d184d2c41433
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2017-06-21 20:44:13 +0100

    Batched image processing

--- apa/migrations/0003_recreate_thumbnails.py
+++ apa/migrations/0003_recreate_thumbnails.py
@@ -23,17 +23,23 @@ def save_thumb(image_path, thumb_path):
 
 def recrate_thumbnails(apps, schema_editor):
     Image = apps.get_model('apa', 'Image')
+    all_images = Image.objects.all()
 
-    for image in Image.objects.all():
-        str_dt = get_datetime(image.datetime).strftime("%Y%m/%d")
-        image_path = "%s/%s.%s" % (str_dt, image.text_id, image.extension or "jpg")
-        image_full_path = os.path.join("images", image_path)
-        thumb_path = os.path.join("thumbs", image_path)
+    cnt = all_images.count()
+    batch_size = 10000
 
-        try:
-            save_thumb(image_full_path, thumb_path)
-        except IOError:
-            print('Missed file: %s' % image_path)
+    for i in range(0, cnt / batch_size + 1):
+        offset = i * batch_size
+        for image in all_images[offset:offset + batch_size]:
+            str_dt = get_datetime(image.datetime).strftime("%Y%m/%d")
+            image_path = "%s/%s.%s" % (str_dt, image.text_id, image.extension or "jpg")
+            image_full_path = os.path.join("images", image_path)
+            thumb_path = os.path.join("thumbs", image_path)
+
+            try:
+                save_thumb(image_full_path, thumb_path)
+            except IOError:
+                print('Missed file: %s' % image_path)
 
 
 class Migration(migrations.Migration):
